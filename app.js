d662b0d509d944afb7c2b410040d750c0002a330
Lucian <lucianw9977@gmail.com>
2023-05-01 20:48:35 -0700

    Name effect added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+
 // Scroll-to-top button:
 document.addEventListener("DOMContentLoaded", function() {
     const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
@@ -34,53 +36,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-// wait for page load
-window.addEventListener('load', () => {
-  // simulate a delay of 5 seconds before hiding the loader
-  setTimeout(() => {
-    const loader = document.querySelector('.loader-wrapper');
-    const body = document.querySelector('body');
-    loader.classList.add('loaded');
-    body.style.overflow = 'visible';
-  }, 5000);
-});
-
-// smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// scroll to top button
-const scrollToTopBtn = document.querySelector("#scroll-to-top-btn");
-window.addEventListener("scroll", () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
-});
-
-scrollToTopBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
-
-
-
 // preloader
 window.addEventListener('load', function () {
     const preloader = document.querySelector('.preloader');
     preloader.classList.add('hide');
     setTimeout(function () {
         preloader.style.display = 'none';
-    }, 6000);
+    }, 4000);
 });
 
 
@@ -88,13 +50,45 @@ window.addEventListener('load', function () {
 
 
 
+// scramble name effect
+function scrambleText(el, text) {
+    const chars = '!<>-_\\/[]{}—=+*^?#________';
+    let scrambledText = '';
 
+    for (let i = 0; i < text.length; i++) {
+        if (Math.random() < 0.5) {
+            scrambledText += text[i];
+        } else {
+            scrambledText += chars[Math.floor(Math.random() * chars.length)];
+        }
+    }
 
+    el.innerHTML = scrambledText;
+}
 
+function updateScramble(el, text, scrambleDuration, pauseDuration) {
+    let scrambleStartTime = Date.now();
+    const scramble = () => {
+        const elapsedTime = Date.now() - scrambleStartTime;
 
+        if (elapsedTime < scrambleDuration) {
+            scrambleText(el, text);
+            requestAnimationFrame(scramble);
+        } else {
+            el.innerHTML = text;
+            setTimeout(() => {
+                scrambleStartTime = Date.now();
+                scramble();
+            }, pauseDuration);
+        }
+    };
 
+    scramble();
+}
 
-
-
+document.addEventListener('DOMContentLoaded', function () {
+    const scrambledName = document.getElementById('scrambled-name');
+    updateScramble(scrambledName, 'lucian wang', 1000, 2500);
+});
 
 
